d546b2341f7eb14bffae1076c7b8bb495b3d7fcc
talundy <tlundy@calpoly.edu>
2023-05-10 15:30:41 -0700

    adding menu navigation and styling

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+
+console.log("statmenet");
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+});
+
+
+
